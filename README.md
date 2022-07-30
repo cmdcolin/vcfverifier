f7292f52bea6f7138191d15797c1667bd26512e2
Colin <colin.diesh@gmail.com>
2022-07-29 18:55:48 -0600

    Use bgzf file reader, otherwise get incomplete https://stackoverflow.com/questions/71239703/flate2-gzdecoder-not-reading-the-whole-file

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # vcfverifier
 
-Checks that a given VCF file matches a given assembly in FASTA format
+Checks that a given VCF file matches a given assembly in FASTA format by
+checking that the REF column matches the FASTA file for each record in the
+FASTA file (case insensitive)
+
+## Install
+
+First install rust, probably with rustup https://rustup.rs/
+
+Then
+
+```
+cargo install --git https://github.com/cmdcolin/vcfverifier
+```
 
 ## Usage
 
@@ -9,7 +21,21 @@ Checks that a given VCF file matches a given assembly in FASTA format
 samtools faidx myfile.fa
 
 ## Run the verifier
-vcfverifier --fasta myfile.fa --vcf myfile.fa.gz
+vcfverifier --fasta myfile.fa --vcf myfile.vcf.gz
+```
+
+Allows plaintext, gzip, or bgzip vcf files as input to the --vcf flag
+
+## Approx speed
+
+Processing chr1 (6.5M rows) of the 1000 genomes dataset takes ~24seconds
+
+```
+$ time vcfverifier --fasta hs37d5.fa --vcf ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz
+Lines processed: 6468347
+No mismatching lines found
+vcfverifier --fasta ~/Downloads/hs37d5.fa --vcf   24.07s user 0.26s system 99% cpu 24.330 total
+
 ```
 
 ## Note
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use clap::Parser;
 use faimm::IndexedFasta;
-use flate2::read::GzDecoder;
+use rust_htslib::bgzf::Reader;
 use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::io;
+use std::io::{BufRead, BufReader, Read};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -20,29 +20,29 @@ fn main() -> Result<(), &'static str> {
     let fa = IndexedFasta::from_file(args.fasta).expect("Error opening fasta file");
     let mut lineno = 0;
     let mut err = 0;
-    if let Ok(lines) = read_lines(args.vcf) {
-        for line in lines {
-            lineno = lineno + 1;
-            if let Ok(ip) = line {
-                if ip.chars().next().unwrap() == '#' {
-                    continue;
-                }
-                let s = ip.to_string();
-                let (chr_name, pos, ref_allele) = get_columns(&s);
-                let chr_index = fa.fai().tid(chr_name).expect("Cannot find chr in index");
-                let v = fa
-                    .view(chr_index, pos, pos + ref_allele.len())
-                    .expect("Cannot get .fa view");
+    let lines = get_reader(&args.vcf.to_string());
 
-                let needle = v.to_string().to_lowercase();
-                let haystack = ref_allele.to_lowercase();
-                if needle != haystack {
-                    println!("Failed line {}: {}", lineno, ip);
-                    err = 1;
-                }
+    for line in lines {
+        lineno = lineno + 1;
+        if let Ok(ip) = line {
+            if ip.chars().next().unwrap() == '#' {
+                continue;
+            }
+            let s = ip.to_string();
+            let (chr_name, pos, ref_allele) = get_columns(&s);
+            let chr_index = fa.fai().tid(chr_name).expect("Cannot find chr in index");
+            let fasta = fa
+                .view(chr_index, pos, pos + ref_allele.len())
+                .expect("Cannot get .fa view")
+                .to_string();
+
+            if fasta.to_lowercase() != ref_allele.to_lowercase() {
+                println!("Failed line {}: {}", lineno, ip);
+                err = 1;
             }
         }
     }
+    println!("Lines processed: {}", lineno);
 
     if err != 0 {
         Err("Found some mismatching lines")
@@ -52,15 +52,25 @@ fn main() -> Result<(), &'static str> {
     }
 }
 
-// from https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html#read_lines
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<flate2::read::GzDecoder<File>>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
+fn get_reader(path: &str) -> io::Lines<BufReader<Box<dyn Read>>> {
+    let file_type = path
+        .split(".")
+        .collect::<Vec<&str>>()
+        .last()
+        .unwrap()
+        .clone();
 
-    let reader = io::BufReader::new(GzDecoder::new(file));
-    Ok(reader.lines())
+    match file_type {
+        "gz" => {
+            let mm: Box<dyn Read> = Box::new(Reader::from_path(path).unwrap());
+            BufReader::new(mm).lines()
+        }
+        _t => {
+            let file = File::open(path).unwrap();
+            let mm: Box<dyn Read> = Box::new(file);
+            BufReader::new(mm).lines()
+        }
+    }
 }
 
 // get chr_name, pos, and ref_allele avoiding splitting the whole line (could be many genotypes)
